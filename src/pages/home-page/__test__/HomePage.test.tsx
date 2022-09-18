498ead56b18ca326f793fd5dee26fa00428e5f4a
RabinThapa1998 <imrabin1998@gmail.com>
2022-09-18 22:30:13 +0545

    add css test

--- src/pages/home-page/__test__/HomePage.test.tsx
+++ src/pages/home-page/__test__/HomePage.test.tsx
@@ -14,6 +14,13 @@ it("renders homepage on the screen", () => {
   const headerElement = screen.getByText(/homepage/i);
   expect(headerElement).toBeInTheDocument();
 });
+it("renders homepage with color black on the screen", () => {
+  render(<HomePage />);
+  const headerElement = screen.getByText(/homepage/i);
+  //   screen.debug();
+  expect(headerElement).toHaveStyle("font-size: 3rem");
+  expect(headerElement).toHaveStyle("font-weight: 400");
+});
 it("renders generate button on the screen", () => {
   render(<HomePage />);
   const btnElement = screen.getByTestId("generate");
