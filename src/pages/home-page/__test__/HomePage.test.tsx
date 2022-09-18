f8d06bfea3f1b41e6384059d45c7ebda4cd14a5a
RabinThapa1998 <imrabin1998@gmail.com>
2022-09-18 22:15:28 +0545

    feat: added integration testing

--- src/pages/home-page/__test__/HomePage.test.tsx
+++ src/pages/home-page/__test__/HomePage.test.tsx
@@ -24,3 +24,19 @@ it("renders age field on the screen", () => {
   const ageTextFieldElement = screen.getByTestId("age-1");
   expect(ageTextFieldElement).toBeInTheDocument();
 });
+
+it("renders generate button click and display card", () => {
+  render(<HomePage />);
+  const ageTextFieldElement = screen.getByTestId("age-1");
+  expect(ageTextFieldElement).toBeInTheDocument();
+  fireEvent.change(ageTextFieldElement, {
+    target: { value: 10 },
+  });
+
+  const btnElement = screen.getByRole("button", { name: "Generate" });
+  fireEvent.click(btnElement);
+  //   screen.debug();
+  const cardElement = screen.getByTestId(0);
+  expect(cardElement).toBeInTheDocument();
+  expect(cardElement).toHaveTextContent("Age: 10");
+});
